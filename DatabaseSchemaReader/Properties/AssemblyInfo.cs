5950f512d1f043a2dcd91ec25411e27c20938f52
HCricle <15218450198@163.com>
2023-03-02 11:31:44 +0800

    Fix net6.0build

--- DatabaseSchemaReader/Properties/AssemblyInfo.cs
+++ DatabaseSchemaReader/Properties/AssemblyInfo.cs
@@ -32,7 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#if !NET6_0
 [assembly: AssemblyVersion("2.7.17.0")]
 [assembly: AssemblyFileVersion("2.7.17.0")]
+#endif
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("DatabaseSchemaReaderTest")]
